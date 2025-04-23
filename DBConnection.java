231c226519c6764f4a50c8af2973fec1694542d7
Sarthak Ukharde <166685802+Sarthak-36@users.noreply.github.com>
2025-04-23 23:16:32 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/student_db";
     private static final String USER = "root";  // Change this if needed
-    private static final String PASSWORD = "Sarthak@3206"; // Change this if needed
+    private static final String PASSWORD = ""; // Change this if needed
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
